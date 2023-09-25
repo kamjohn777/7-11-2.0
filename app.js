5434ab873cc3af77bf9fdd6280c4ccd2ff749218
kamjohn777 <kamjohn79.kdj@gmail.com>
2023-09-24 23:51:36 -0500

    added an html and css file to project so created a front end

--- app.js
+++ app.js
@@ -5,8 +5,32 @@
     let currentRollResults = [];
     let rollResult = 0;
     let establishedPoint = null;
+
+    // test from codepen
+    var mainScores, currentScore, activePlayer, gamePlaying;
+
+    function nextPlayer() {
+        // below will execute next player turn if player rolls dice equal to 1
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // same as saying if activePlayer === 0 then activePlayer === 1 else activePlayer = 0
+        currentScore = 0; //resets players current score box to 0
+    ​
+        document.getElementById("current-0").textContent = 0; //if player 1 rolls a one then score = 0
+        document.getElementById("current-1").textContent = 0; //if player 2 rolls a one then score = 0
+      
+        document.querySelector(".player-0-panel").classList.toggle("active"); //toggles to player 2
+        document.querySelector(".player-1-panel").classList.toggle("active"); //toggles back to player 1
+    ​
+        //once player rolls a 1 the dice will disappear and it will be next player turn
+        document.querySelector(".dice").style.display = "none";
+    }
+    // end of test from codepen
    
     function diceRoll() {
+        let name1 = document.querySelector(".body .row-2 .player-1 > input").value;
+        let name2 = document.querySelector(".body .row-2 .player-2 > input").value;
+        if(name1==""||name2=="") {
+          document.querySelector(".result").innerHTML = "Fill in names of players";
+        } else {
     
       const dice1 = Math.floor(Math.random() * 6) + 1;
       const dice2 = Math.floor(Math.random() * 6) + 1;
@@ -16,6 +40,7 @@
       rollResult = sum;
       checkDice();
       return sum;
+        }
     }
 
     function switchPlayer(currentPlayerName) {
@@ -60,13 +85,13 @@
             if (rollResult === 7 || rollResult === 11) {
                 players[currentRoller].score += 1;
                 currentRoller = 0;
-                // console.log(`${players[currentRoller].name} won this round they rolled a ${rollResult}`);
+                console.log(`${players[currentRoller].name} won this round they rolled a ${rollResult}`);
                 // return `${players[currentRoller].name} won this round they rolled a ${rollResult} ${players[currentRoller].name} rolls again`
                return result = `${players[currentRoller].name} won this round they rolled a ${rollResult} ${players[currentRoller].name} rolls again`;
             } else if (rollResult === 2 || rollResult === 3 || rollResult === 12) {
                 players[currentRoller].score -= 1;
                 currentRoller = 1;
-                // console.log(`${players[currentRoller].name} loss this round they rolled a ${rollResult}`);
+                console.log(`${players[currentRoller].name} loss this round they rolled a ${rollResult}`);
                 // return `${players[currentRoller].name} loss this round they rolled a ${rollResult} ${players[currentRoller].name}'s turn is over`
                 return result = `${players[currentRoller].name} loss this round they rolled a ${rollResult} ${players[currentRoller].name}'s turn is over`;
                 // changeTurns();
@@ -95,7 +120,7 @@
 
   
     console.log("Next player: " + switchPlayer('austin')); // This will return 'kam'
-console.log("Next player: " + switchPlayer('kam')); // This will return 'austin'
+// console.log("Next player: " + switchPlayer('kam')); // This will return 'austin'
 // console.log("Next player: " + switchPlayer('unknown')); // This will return 'unknown'
 // console.log("this is the diceroll function:" ,diceRoll())
 // console.log("This is the currentRollResulet", currentRollResults);
